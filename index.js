c31c9f6b93886694bfbfef4ac43adf1cd6edfa1a
Matthew <matthew.d.baker20@gmail.com>
2021-11-22 17:14:32 -0600

    working on rendering a board from a Fen string from chess api, issues at the moment with the square colors... no idea. The board renders really well tho and can cycle games based off username

--- index.js
+++ index.js
@@ -2,6 +2,68 @@
 
     const board = $("#board");
     const body = $("body");
+    let turn = 0;
+    let cache = [];
+    let gameData = [];
+    let idx = 0;
+    let notActive = true;
+
+    let whoAndWho = document.getElementById("whoAndWho");
+    let next = document.getElementById("next");
+    let prev = document.getElementById("prev");
+    let gameIdx = document.getElementById("gameIdx");
+    let userNameInput = document.getElementById("username");
+    document.getElementById("submit").addEventListener("click", () => {
+        submit();
+
+    });
+
+    window.addEventListener("keydown", e => {
+        if(e.key === "Enter") {
+            submit();
+        }
+    });
+
+    function submit() {
+        let input = userNameInput.value;
+        if(input.trim().length > 0) {
+            // need to validate the response, valid username with chess.com and valid games
+            // TODO: this is causing event bubbling...... need to clean up the logic
+            init(input).then(buttons);
+        } else {
+            console.log("enter a user name");
+        }
+    }
+
+    // TODO: SOME IDX RESETTING OUTSIDE THIS AND USE THIS BOOL TO PREVENT BUBBLING...for now..
+    function buttons() {
+        if(notActive) {
+            let res = getPositionFromFen(board, cache[idx], createBoard);
+            if(res === -1) {
+                document.getElementById("loading").innerText = "not found";
+                return;
+            }
+            document.getElementById("loading").style.display = "none";
+            next.addEventListener("click", () => {
+                idx++;
+                idx %= cache.length;
+                getPositionFromFen(board, cache[idx]);
+                gameIdx.innerText = (idx + 1).toString();
+                whoAndWho.innerText = gameData[idx];
+            });
+            prev.addEventListener("click", () => {
+                idx--;
+                idx = idx < 0 ? cache.length - 1 : idx;
+                getPositionFromFen(board, cache[idx]);
+                gameIdx.innerText = (idx + 1).toString();
+                whoAndWho.innerText = gameData[idx];
+            });
+            gameIdx.innerText = (idx + 1).toString();
+            whoAndWho.innerText = gameData[idx];
+        }
+        notActive = false; // fix?
+        console.log("Ready");
+    }
 
     function createBoard(){
         board.html("");
@@ -13,53 +75,61 @@
     }
 
     createBoard();
-
-    function square(row, col) {
-        let isBlack = (row + col) % 2 !== 0;
-        let color = isBlack ? "#53382c" : "#8bb1c3";
-        let squareAttr = isBlack ? "black" : "white";
-        let square = document.createElement("div");
-        let pieceType = pieceLetter(row, col);
-        square.setAttribute("data-val", pieceType);
-        square.setAttribute("sq-color", squareAttr);
-        square.setAttribute("position", `${row}-${col}`);
-        square.classList.add("square");
-        square.innerHTML = pieceType;
-        square.style.backgroundColor = color;
-        square.style.width = "100px";
-        square.style.height = "100px";
-        return square;
+    function url(name) {
+        return `https://api.chess.com/pub/player/${name}/games`;
     }
 
-    function pieceLetter(row, col) {
-        let prefix = row < 2 ? "b-" : "w-";
-        if(row === 1 || row === 6) {
-            return `<img src="img/${prefix}pawn.png">`;
-        }
-        if(row === 7 || row === 0) {
-            if(col === 0 || col === 7) {
-                return `<img src="img/${prefix}rook.png">`;
-            }
-            if(col === 1 || col === 6) {
-                return `<img src="img/${prefix}knight.png">`;
-            }
-            if(col === 2 || col === 5) {
-                return `<img src="img/${prefix}bishop.png">`;
-            }
-
-            // these need to change for white / black
-            if(col === 3) {
-                return `<img src="img/${prefix}queen.png">`;
-            }
-            if(col === 4) {
-                return `<img src="img/${prefix}king.png">`;
-            }
-        }
-        return "";
+    async function init(name) {
+        cache = [];
+        gameData = [];
+        const games = await getData(name);
+        games.forEach(game => {
+            cache.push(formatFen(game.fen));
+            gameData.push(getPlayerNames(game.white.split("/"), game.black.split("/")));
+        });
+        return cache;
     }
 
+    function formatFen(fen) {
+        return fen.split(" ")[0];
+    }
+
+    // get the colors to for each
+    function getPlayerNames(nameString1, nameString2) {
+        let one = nameString1[nameString1.length - 1];
+        let two = nameString2[nameString2.length - 1];
+        return `${one} as W and ${two} as B`;
+    }
+
+    function getData(name) {
+        return fetch(url(name)).then(res => res.json().then(data => {
+            return data.games
+        }));
+    }
+
+    // getPositionFromFen(board, 'r4rk1/8/2p5/p3Pp2/PpQ4n/8/1PP2P2/2KR4');
+
+
+
+
+    /* this is the potential cause for the moves, as I click a "new" square
+        that square now has the html assigned to it, the piece that was just moved to it
+        and now there are possible moves for that piece
+        --maybe a turn handler will fix this?
+    */
     body.on("click", ".square", function () {
         let t = $(this);
+
+        // will use later
+        console.log(squareIsOccupied(t));
+
+
+        turn++;
+        if(turn % 2 === 0) return; // blacks turn instead so same ish logic just
+                                    // a shitty fix for the constant movement problem
+
+        // this can be passed into the moveTypeFromPiece instead to generate moves for black rather
+
         let pieceType = t.attr("data-val");
         let currentPosition = t.attr("position");
         // if(!moveTypeFromPiece(pieceType, currentPosition)) return;
@@ -131,10 +201,27 @@
     }
 
     function knightMoveTest(piece, data) {
+        /* for wherever a knight is
+            it's moves are based of bounds on the board
+            create the 8 moves, add the ones within bounds to the return
+            if anything over 7 x 7 do not add
+            take the current square and loop again... n^2 just in this
+            find squares with
+        */
+
+        console.log(data.currSquare.getAttribute("position"));
+        let allSquares = Array.from(document.getElementsByClassName("square"));
+
+        for(let i = 0; i < allSquares.length; i++) {
+            let curr = allSquares[i];
+            let position = curr.getAttribute("position");
+        }
 
     }
 
     function pawnMoveWhiteTest(piece, data){
+        // if curr square is still starting and no obstruction, allow for double push start
+
         // white pawn can move one up and only in this column
         if (piece.color === WHITE && +data.thisRow + 1 === +data.currRow && data.currCol === data.thisCol) {
             let classToAdd = data.sqColor === "white" ? "can-move-to-white" : "can-move-to-black";
--- util.js
+++ util.js
@@ -10,6 +10,86 @@ const PIECES = {
 const WHITE = "w";
 const BLACK = "b";
 
+// function square(row, col, pieceLetterInput = '') {
+//     let isBlack = (row + col) % 2 !== 0;
+//     let color = isBlack ? "#53382c" : "#8bb1c3";
+//     let squareAttr = isBlack ? "black" : "white";
+//     let square = document.createElement("div");
+//     let pieceType = pieceLetterInput ? getPiece(pieceLetterInput) : pieceLetter(row, col);
+//     square.setAttribute("data-val", pieceType);
+//     square.setAttribute("sq-color", squareAttr);
+//     square.setAttribute("position", `${row}-${col}`);
+//     square.classList.add("square");
+//     square.innerHTML = pieceType;
+//     square.style.backgroundColor = color;
+//     square.style.width = "100px";
+//     square.style.height = "100px";
+//     return square;
+// }
+
+function square(row, col, pieceLetterInput = '', fromPosition = false) {
+    let isBlack = (row + col) % 2 !== 0;
+    let color = isBlack ? "#53382c" : "#8bb1c3";
+    let squareAttr = isBlack ? "black" : "white";
+    let square = document.createElement("div");
+    let pieceType = fromPosition ? getPiece(pieceLetterInput) : pieceLetter(row, col);
+    square.setAttribute("data-val", pieceType);
+    square.setAttribute("sq-color", squareAttr);
+    square.setAttribute("position", `${row}-${col}`);
+    square.classList.add("square");
+    square.innerHTML = pieceType ? pieceType : '';
+    square.style.backgroundColor = color;
+    square.style.width = "100px";
+    square.style.height = "100px";
+    return square;
+}
+
+function getPiece(pieceInput) {
+    let [color, pieceType] = pieceInput.split("_");
+
+    switch (pieceType) {
+        case "p":
+            return `<img alt="piece" src="img/${color}pawn.png">`;
+        case "r":
+            return `<img alt="piece" src="img/${color}rook.png">`;
+        case "n":
+            return `<img alt="piece" src="img/${color}knight.png">`;
+        case "b":
+            return `<img alt="piece" src="img/${color}bishop.png">`;
+        case "q":
+            return `<img alt="piece" src="img/${color}queen.png">`;
+        case "k":
+            return `<img alt="piece" src="img/${color}king.png">`;
+    }
+}
+
+function pieceLetter(row, col) {
+    let prefix = row < 2 ? "b-" : "w-";
+    if(row === 1 || row === 6) {
+        return `<img alt="piece" src="img/${prefix}pawn.png">`;
+    }
+    if(row === 7 || row === 0) {
+        if(col === 0 || col === 7) {
+            return `<img alt="piece" src="img/${prefix}rook.png">`;
+        }
+        if(col === 1 || col === 6) {
+            return `<img alt="piece" src="img/${prefix}knight.png">`;
+        }
+        if(col === 2 || col === 5) {
+            return `<img alt="piece" src="img/${prefix}bishop.png">`;
+        }
+
+        // these need to change for white / black
+        if(col === 3) {
+            return `<img alt="piece" src="img/${prefix}queen.png">`;
+        }
+        if(col === 4) {
+            return `<img alt="piece" src="img/${prefix}king.png">`;
+        }
+    }
+    return "";
+}
+
 function parsePieceFromImgString(imgString) {
     const [color, piece] = imgString.split("/")[1].split(".")[0].split("-");
     return {
@@ -19,7 +99,7 @@ function parsePieceFromImgString(imgString) {
 }
 
 function squareIsOccupied(square) {
-
+    return square[0].dataset.val.length > 0;
 }
 
 function removePieceAndMoveHighlightFromPrevSquareAndUpdateDataSetVal(col, row, board, tgtSquare) {
@@ -50,4 +130,58 @@ function removePieceAndMoveHighlightFromPrevSquareAndUpdateDataSetVal(col, row, 
 function removeHighlightClass(domSquare) {
     domSquare.classList.remove("can-move-to-black");
     domSquare.classList.remove("can-move-to-white");
+}
+
+
+function getPositionFromFen(board, fenString, fallBackFn) {
+    board.html("");
+    // from invalid username, call normal create board function
+    if(!fenString) {
+        fallBackFn(board);
+        return -1;
+    }
+    console.log(fenString);
+//   r4rk1/8/2p5/p3Pp2/PpQ4n/8/1PP2P2/2KR4
+    let currentPieceFromFen = '';
+    let fenIdx = 0;
+
+    for(let rows = 0; rows < 8; rows++) {
+        for(let cols = 0; cols < 8; cols++) {
+            let curr = fenString[fenIdx];
+            if(!curr) return;
+            if(isNaN(parseInt(curr))) {
+                if (curr === "/") {
+                    fenIdx++;
+                    continue;
+                }
+                currentPieceFromFen = curr === curr.toLowerCase()
+                    ? `${BLACK}-_${curr}`
+                    : `${WHITE}-_${curr.toLowerCase()}`;
+                board.append(square(rows, cols, currentPieceFromFen, true));
+            } else {
+                let [newRows, newCols] = createBlankSquares(board, rows, cols, parseInt(curr));
+                cols = newCols;
+                rows = newRows;
+            }
+            console.log([rows, cols]);
+            fenIdx++;
+        }
+    }
+
+}
+
+// i think the cols amount is not right when it leaves this recursive call...
+// everytime a piece is hit the color messes up
+function createBlankSquares(board, rows, cols, amountOfBlankSquares) {
+    if(cols > 7) {
+        cols = 0;
+        rows++;
+    }
+    console.log([rows, cols]);
+    if(amountOfBlankSquares <= 1) {
+        board.append(square(rows, cols, '', true));
+        return [rows, cols];
+    }
+    board.append(square(rows, cols, '', true));
+    return createBlankSquares(board, rows, cols + 1, amountOfBlankSquares - 1);
 }
