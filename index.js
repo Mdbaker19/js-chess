9185e1f90b34d7202a87b1f0f31e1fd72579bce2
Matthew <matthew.d.baker20@gmail.com>
2021-11-23 20:09:09 -0600

    fixed color issue, added color change option, need to work on piece move logic

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 (() => {
-
     const board = $("#board");
-    const body = $("body");
-    let turn = 0;
     let cache = [];
     let gameData = [];
     let idx = 0;
@@ -15,7 +12,12 @@
     let userNameInput = document.getElementById("username");
     document.getElementById("submit").addEventListener("click", () => {
         submit();
+    });
 
+    document.getElementById("changeMe").addEventListener("click", () => {
+        whiteColor = whiteColorInput.value;
+        blackColor = blackColorInput.value;
+        getPositionFromFen(board, currentFen, createBoard);
     });
 
     window.addEventListener("keydown", e => {
@@ -74,7 +76,8 @@
         }
     }
 
-    createBoard();
+    getPositionFromFen(board, pieceTestFen, createBoard);
+
     function url(name) {
         return `https://api.chess.com/pub/player/${name}/games`;
     }
@@ -105,131 +108,6 @@
         return fetch(url(name)).then(res => res.json().then(data => {
             return data.games
         }));
-    }
-
-    // getPositionFromFen(board, 'r4rk1/8/2p5/p3Pp2/PpQ4n/8/1PP2P2/2KR4');
-
-
-
-
-    /* this is the potential cause for the moves, as I click a "new" square
-        that square now has the html assigned to it, the piece that was just moved to it
-        and now there are possible moves for that piece
-        --maybe a turn handler will fix this?
-    */
-    body.on("click", ".square", function () {
-        let t = $(this);
-
-        // will use later
-        console.log(squareIsOccupied(t));
-
-
-        turn++;
-        if(turn % 2 === 0) return; // blacks turn instead so same ish logic just
-                                    // a shitty fix for the constant movement problem
-
-        // this can be passed into the moveTypeFromPiece instead to generate moves for black rather
-
-        let pieceType = t.attr("data-val");
-        let currentPosition = t.attr("position");
-        // if(!moveTypeFromPiece(pieceType, currentPosition)) return;
-        // console.log(pieceType);
-        // console.log(currentPosition);
-        moveTypeFromPiece(pieceType, currentPosition);
-    });
-
-    function moveTypeFromPiece(piece, currentPosition) {
-        if(!piece) return false;
-        let [row, col] = currentPosition.split("-");
-        let movesList = generateMoves(piece, currentPosition, row, col);
-        allowMoves(movesList, piece, row, col);
-    }
-
-    // so currently event bubbles....
-    function allowMoves(movesList, piece, currRow, currCol) {
-        console.log(movesList);
-        movesList.forEach(move => {
-            move.addEventListener("click", () => {
-                move.innerHTML = piece;
-                removePieceAndMoveHighlightFromPrevSquareAndUpdateDataSetVal(currCol, currRow, board, move);
-                // need to count a turn
-                // the dataSet value needs to be removed?
-                // if i click on a square that was prev occupied by the pawn it thinks the
-                // pawn is still there
-            });
-        });
-    }
-
-    function generateMoves(piece, currentPosition, currRow, currCol) {
-        piece = parsePieceFromImgString(piece);
-        // get all the squares, find the ones that have a position that is "reachable" add a appearance to them
-        let allSquares = Array.from(document.getElementsByClassName("square"));
-        let possibleMoves = [];
-        for(let i = 0; i < allSquares.length; i++) {
-            let currSquare = allSquares[i];
-
-            // get the row and col for a potential target square
-            let [thisRow, thisCol] = currSquare.getAttribute("position").split("-");
-            let sqColor = currSquare.getAttribute("sq-color");
-
-            let data = {currSquare, thisRow, thisCol, sqColor, currRow, currCol};
-
-            // move comes back valid ? use it or skip
-            let move = moveOptionsByPiece(piece, data);
-            move ? possibleMoves.push(move) : undefined;
-
-        }
-        return possibleMoves;
-    }
-
-    // others require square blocking check
-    function moveOptionsByPiece(piece, data) {
-        switch (piece.piece) {
-            case PIECES.PAWN:
-                return pawnMoveWhiteTest(piece, data);
-            case PIECES.ROOK:
-                return;
-            case PIECES.KNIGHT:
-                return knightMoveTest(piece, data);
-            case PIECES.BISHOP:
-                return;
-            case PIECES.QUEEN:
-                return;
-            case PIECES.KING:
-                return;
-        }
-    }
-
-    function knightMoveTest(piece, data) {
-        /* for wherever a knight is
-            it's moves are based of bounds on the board
-            create the 8 moves, add the ones within bounds to the return
-            if anything over 7 x 7 do not add
-            take the current square and loop again... n^2 just in this
-            find squares with
-        */
-
-        console.log(data.currSquare.getAttribute("position"));
-        let allSquares = Array.from(document.getElementsByClassName("square"));
-
-        for(let i = 0; i < allSquares.length; i++) {
-            let curr = allSquares[i];
-            let position = curr.getAttribute("position");
-        }
-
-    }
-
-    function pawnMoveWhiteTest(piece, data){
-        // if curr square is still starting and no obstruction, allow for double push start
-
-        // white pawn can move one up and only in this column
-        if (piece.color === WHITE && +data.thisRow + 1 === +data.currRow && data.currCol === data.thisCol) {
-            let classToAdd = data.sqColor === "white" ? "can-move-to-white" : "can-move-to-black";
-            data.currSquare.classList.add(classToAdd);
-
-            // added to later be used to loop through for allowable clicks to transfer piece to
-            return data.currSquare;
-        }
     }
 
 })();
--- util.js
+++ util.js
@@ -1,3 +1,5 @@
+let currentFen = "rbnqknbr/pppppppp/8/8/8/8/PPPPPPPP/RBNQKNBR";
+let pieceTestFen = "8/pb3k2/1PBn4/8/8/Q7/6r1/8"
 const PIECES = {
     PAWN: "pawn",
     ROOK: "rook",
@@ -10,29 +12,18 @@ const PIECES = {
 const WHITE = "w";
 const BLACK = "b";
 
-// function square(row, col, pieceLetterInput = '') {
-//     let isBlack = (row + col) % 2 !== 0;
-//     let color = isBlack ? "#53382c" : "#8bb1c3";
-//     let squareAttr = isBlack ? "black" : "white";
-//     let square = document.createElement("div");
-//     let pieceType = pieceLetterInput ? getPiece(pieceLetterInput) : pieceLetter(row, col);
-//     square.setAttribute("data-val", pieceType);
-//     square.setAttribute("sq-color", squareAttr);
-//     square.setAttribute("position", `${row}-${col}`);
-//     square.classList.add("square");
-//     square.innerHTML = pieceType;
-//     square.style.backgroundColor = color;
-//     square.style.width = "100px";
-//     square.style.height = "100px";
-//     return square;
-// }
+let blackColorInput = document.getElementById("black-color");
+let blackColor = blackColorInput.value;
+let whiteColorInput = document.getElementById("white-color");
+let whiteColor = whiteColorInput.value;
 
-function square(row, col, pieceLetterInput = '', fromPosition = false) {
+function square(row, col, pieceLetterInput = '') {
     let isBlack = (row + col) % 2 !== 0;
-    let color = isBlack ? "#53382c" : "#8bb1c3";
+    console.log(blackColor, whiteColor);
+    let color = isBlack ? blackColor : whiteColor;
     let squareAttr = isBlack ? "black" : "white";
     let square = document.createElement("div");
-    let pieceType = fromPosition ? getPiece(pieceLetterInput) : pieceLetter(row, col);
+    let pieceType = getPiece(pieceLetterInput);
     square.setAttribute("data-val", pieceType);
     square.setAttribute("sq-color", squareAttr);
     square.setAttribute("position", `${row}-${col}`);
@@ -63,85 +54,17 @@ function getPiece(pieceInput) {
     }
 }
 
-function pieceLetter(row, col) {
-    let prefix = row < 2 ? "b-" : "w-";
-    if(row === 1 || row === 6) {
-        return `<img alt="piece" src="img/${prefix}pawn.png">`;
-    }
-    if(row === 7 || row === 0) {
-        if(col === 0 || col === 7) {
-            return `<img alt="piece" src="img/${prefix}rook.png">`;
-        }
-        if(col === 1 || col === 6) {
-            return `<img alt="piece" src="img/${prefix}knight.png">`;
-        }
-        if(col === 2 || col === 5) {
-            return `<img alt="piece" src="img/${prefix}bishop.png">`;
-        }
-
-        // these need to change for white / black
-        if(col === 3) {
-            return `<img alt="piece" src="img/${prefix}queen.png">`;
-        }
-        if(col === 4) {
-            return `<img alt="piece" src="img/${prefix}king.png">`;
-        }
-    }
-    return "";
-}
-
-function parsePieceFromImgString(imgString) {
-    const [color, piece] = imgString.split("/")[1].split(".")[0].split("-");
-    return {
-        color,
-        piece
-    }
-}
-
-function squareIsOccupied(square) {
-    return square[0].dataset.val.length > 0;
-}
-
-function removePieceAndMoveHighlightFromPrevSquareAndUpdateDataSetVal(col, row, board, tgtSquare) {
-    // get the board and turn to an 8x8 2d arr to then remove the prev html pic
-    const boardData = board[0].children;
-    let boardCopy = [];
-    for(let i = 0; i < 8; i++) {
-        let rowArr = [];
-        for(let j = 0; j < 8; j++) {
-            let boardIdx = i * 8 + j;
-            rowArr.push(boardData[boardIdx]);
-            removeHighlightClass(boardData[boardIdx]);
-        }
-        boardCopy.push(rowArr);
-    }
-    boardCopy[row][col].innerHTML = "";
-
-    // remove this from the square you moved from, needs to be added to the square you went to...
-    // this is the 'piece' in most of the functions, the html img tag
-
-    // does not work, currently allows the pawn to just storm up the board, adding the highlight class to the next square
-    // in line....
-    // a new pawn can be created behind the one you just moved and that one can also move...wtf
-    tgtSquare.dataset.val = boardCopy[row][col].dataset.val;
-    boardCopy[row][col].dataset.val = "";
-}
-
-function removeHighlightClass(domSquare) {
-    domSquare.classList.remove("can-move-to-black");
-    domSquare.classList.remove("can-move-to-white");
-}
 
 
 function getPositionFromFen(board, fenString, fallBackFn) {
     board.html("");
+    fenString = fenString.split("/").reverse().join("");
     // from invalid username, call normal create board function
     if(!fenString) {
         fallBackFn(board);
         return -1;
     }
-    console.log(fenString);
-//   r4rk1/8/2p5/p3Pp2/PpQ4n/8/1PP2P2/2KR4
+    currentFen = fenString;
     let currentPieceFromFen = '';
     let fenIdx = 0;
 
@@ -150,20 +73,15 @@ function getPositionFromFen(board, fenString, fallBackFn) {
             let curr = fenString[fenIdx];
             if(!curr) return;
             if(isNaN(parseInt(curr))) {
-                if (curr === "/") {
-                    fenIdx++;
-                    continue;
-                }
                 currentPieceFromFen = curr === curr.toLowerCase()
                     ? `${BLACK}-_${curr}`
                     : `${WHITE}-_${curr.toLowerCase()}`;
-                board.append(square(rows, cols, currentPieceFromFen, true));
+                board.append(square(rows, cols, currentPieceFromFen));
             } else {
                 let [newRows, newCols] = createBlankSquares(board, rows, cols, parseInt(curr));
                 cols = newCols;
                 rows = newRows;
             }
-            console.log([rows, cols]);
             fenIdx++;
         }
     }
@@ -177,11 +95,12 @@ function createBlankSquares(board, rows, cols, amountOfBlankSquares) {
         cols = 0;
         rows++;
     }
-    console.log([rows, cols]);
     if(amountOfBlankSquares <= 1) {
-        board.append(square(rows, cols, '', true));
+        board.append(square(rows, cols, ''));
         return [rows, cols];
     }
-    board.append(square(rows, cols, '', true));
+    board.append(square(rows, cols, ''));
     return createBlankSquares(board, rows, cols + 1, amountOfBlankSquares - 1);
 }
+
+// after notes are submitted by the user, save the FEN, possibly to a DB or something by user
